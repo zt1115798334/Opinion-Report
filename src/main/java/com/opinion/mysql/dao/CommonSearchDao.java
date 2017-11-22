d50568b98b45799a1d936c54df625012ca8f7f80
zhangtong <zhangtong9498@qq.com>
2017-11-22 13:48:51 +0800

    msg

--- src/main/java/com/opinion/mysql/dao/CommonSearchDao.java
+++ src/main/java/com/opinion/mysql/dao/CommonSearchDao.java
@@ -1,9 +1,7 @@
 package com.opinion.mysql.dao;
 
-import com.opinion.mysql.entity.ReportArticle;
 import com.opinion.mysql.entity.SysRole;
 import com.opinion.mysql.entity.SysUser;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -14,6 +12,7 @@ public interface CommonSearchDao {
 
     /**
      * 根据账户id查询权限
+     *
      * @param userId 用户id
      * @return
      */
@@ -21,6 +20,7 @@ public interface CommonSearchDao {
 
     /**
      * 根据角色id查询用户信息
+     *
      * @param roleId 角色id
      * @return
      */
