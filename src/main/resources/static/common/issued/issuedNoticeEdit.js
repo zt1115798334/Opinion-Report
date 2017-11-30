4a49466f3f58faee466a41ed126e6bd8fe00c699
zhangtong <zhangtong9498@qq.com>
2017-11-30 14:50:59 +0800

    msg

--- src/main/resources/static/common/issued/issuedNoticeEdit.js
+++ src/main/resources/static/common/issued/issuedNoticeEdit.js
@@ -1,6 +1,104 @@
 $(function () {
 
+    //实例化编辑器
+    var E = window.wangEditor;
+    var editor = new E('#editor');
+    editor.customConfig.uploadImgServer = '/wangEditor/upload'  // 上传图片到服务器
+    editor.customConfig.uploadFileName = 'file'
+    editor.customConfig.uploadImgHeaders = {
+        'Accept': 'application/json'
+    }
+    editor.customConfig.debug = true;
+    editor.create();
+
+    /**
+     * 点击添加操作
+     */
+    validateFun();
+    $(document).on("click", ".sureBtn", function () {
+        var bv = $("#issuedNoticeForm").data('bootstrapValidator');
+        bv.validate();
+        if (!bv.isValid()) {
+            return false;
+        }
+        var data = $("#issuedNoticeForm").serializeJSON();
+        var noticeContentHtml = editor.txt.html();
+        var noticeContentText = editor.txt.text();
+        if (noticeContentText.length == 0) {
+            alert("你还没有填写内容");
+            return false;
+        }
+        if (noticeContentText.length > 1000) {
+            alert("内容长度不能超过1000字");
+            return false;
+        }
+        data.noticeContent = noticeContentHtml;
+        saveIssuedNoticeFun(data, editor);
+    });
+
 });
+function validateFun() {
+    $("#opinionReportForm").bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            // valid: 'glyphicon glyphicon-ok',
+            // invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            title: {
+                validators: {
+                    notEmpty: {
+                        message: '请输入标题'
+                    }, stringLength: {
+                        max: 50,
+                        message: '请输入50个字符'
+                    }
+                }
+            },
+            sourceType: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择来源'
+                    }
+                }
+            },
+            reportLevel: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择等级'
+                    }
+                }
+            },
+            replyType: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择影响范围'
+                    }
+                }
+            },
+            replyNumber: {
+                validators: {
+                    notEmpty: {
+                        message: '请输入数值'
+                    },
+                    regexp: {
+                        regexp: /^\+?(0|[1-9][0-9]*)$/,
+                        message: '只能填写数字'
+                    }
+                }
+            },
+            sourceUrl: {
+                validators: {
+                    regexp: {
+                        regexp: /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/,
+                        message: '请填写正确的URL地址（http或者https开头）'
+                    }
+                }
+            }
+        }
+    });
+}
 
 function saveIssuedNoticeFun(params, editor) {
     var url = "/issuedNotice/saveIssuedNotice";
@@ -9,7 +107,9 @@ function saveIssuedNoticeFun(params, editor) {
 
     function callback(result) {
         if (result.success) {
-
+            alert(result.data.msg);
+            editor.txt.html('');
+            resetForm("issuedNoticeForm");
         }
     }
 }
