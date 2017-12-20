815e6a26ba529930b2fe62025be8becbec0f67f3
zhangtong <zhangtong9498@qq.com>
2017-12-20 14:57:30 +0800

    msg

--- src/main/java/zk/jni/JavaToBiokey.java
+++ src/main/java/zk/jni/JavaToBiokey.java
@@ -44,18 +44,6 @@ public class JavaToBiokey {
 			int AOneToOneThreshold);
 
     static {
-//		System.loadLibrary("matchdll");
-//        String path = "/application.properties";
-//        String path = System.getProperty("user.dir") + File.separator + "application.properties";
-//        System.out.println("path = " + path);
-//        Resource resource = new PathResource(path);
-//        try {
-//            props = PropertiesLoaderUtils.loadProperties(resource);
-//        } catch(IOException e) {
-//            e.printStackTrace();
-//        }
-//        System.load(props.getProperty("path.matchdll"));
-//        System.load("D:\\log\\matchdll.dll");
-        System.load("C:\\Program Files (x86)\\FPOnline\\bin\\matchdll.dll");
+		System.loadLibrary("matchdll");
     }
 }
