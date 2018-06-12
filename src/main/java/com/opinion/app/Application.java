22861135a89965b80c418315c549be8163758482
zt1115798334 <zhangtong9498@qq.com>
2018-06-12 16:25:10 +0800

    msg

--- src/main/java/com/opinion/app/Application.java
+++ src/main/java/com/opinion/app/Application.java
@@ -3,14 +3,10 @@ package com.opinion.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * @author zhangtong
@@ -19,7 +15,6 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @ComponentScan("com.opinion")
 @EntityScan(basePackages = "com.opinion.mysql.entity")
 @EnableJpaRepositories(basePackages = "com.opinion.mysql.repository")
-@EnableMongoRepositories(basePackages = "com.opinion.mongodb.repository")
 @EnableAutoConfiguration
 @EnableScheduling
 public class Application {
